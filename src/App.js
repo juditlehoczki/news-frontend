4f20fc44c5c6b981e465353047e47f6fe13e2ddb
juditlehoczki <lehoczki.judit@gmail.com>
2020-03-16 13:50:32 +0000

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Router } from '@reach/router';
+
+import ArticlesAll from './components/ArticlesAll';
+
+import S from './components/StyleComponents';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <S.AppContainer>
+      <S.Header>Press JL</S.Header>
+      <Router>
+        <ArticlesAll path="/" />
+      </Router>
+    </S.AppContainer>
   );
 }
 
---
+++ src/components/StyleComponents.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const S = {};
+
+S.AppContainer = styled.div`
+  text-align: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+`;
+
+S.Header = styled.h1`
+  border: 1px solid black;
+  margin: 0px;
+  font-size: 1.5em;
+`;
+
+export default S;
