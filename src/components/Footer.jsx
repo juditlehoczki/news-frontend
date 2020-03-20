9169bb488100437f888660f57a5e9084aa2e223c
juditlehoczki <lehoczki.judit@gmail.com>
2020-03-20 12:07:50 +0000

    Using target='_blank' without rel='noopener noreferrer' React warning resolved

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,15 +6,23 @@ const Footer = () => {
   return (
     <S.Footer>
       Created by{" "}
-      <a href="https://github.com/juditlehoczki" target="_blank">
+      <a
+        href="https://github.com/juditlehoczki"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Judit Lehoczki
       </a>{" "}
       for the Frontend Block Review of{" "}
-      <a href="https://northcoders.com/developer-pathway" target="_blank">
+      <a
+        href="https://northcoders.com/developer-pathway"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Developers Pathway
       </a>{" "}
       at{" "}
-      <a href="https://northcoders.com/" target="_blank">
+      <a href="https://northcoders.com/" target="_blank" rel="noopener">
         Northcoders
       </a>
       .
